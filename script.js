6139c257d30aab97f720aa0d74eef96431639750
YusufRiwan <168787966+YusufRiwan@users.noreply.github.com>
2024-10-01 20:46:49 +0100

    Add files via upload
    Todolist which show activities which keep track of your actvitities

---
+++ script.js
@@ -0,0 +1,48 @@
+
+const todo =[{
+    name: 'wash dishes',
+    dueDate: '2024-10-01'
+},{
+    name: 'play football',
+    dueDate: '2024-10-02'
+}
+];
+
+renderTodoList()
+function renderTodoList(){
+
+    let renderTodo = '';
+    for( let i=0; i<todo.length; i++){
+      const todolist = todo[i];
+      const {name,dueDate} = todolist;
+
+      const html = `
+      <div> ${name} </div>
+      <div> ${dueDate}</div>
+       <button class ="delete-btn"
+        onclick ="todo.splice(${i}, 1); renderTodoList()">
+        delete
+        </button>` ;
+      console.log(html);
+      renderTodo += html;
+
+      
+    }
+    document.querySelector('.todo-display').
+ innerHTML = renderTodo;
+    
+}
+
+
+function addTodo(){
+    const todolist = document.querySelector('.todo-list');
+    const name = todolist.value;
+    const date = document.querySelector('.dates');
+    const dueDate = date.value;
+    todo.push({
+        name,
+        dueDate
+    })
+    todo.value = '';
+    renderTodoList();
+};
